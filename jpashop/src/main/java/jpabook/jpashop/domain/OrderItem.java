6108ba0e81dc22141e327a417905ce24fb8a2d19
soohyunnn <sksmsqodn20@gmail.com>
2023-05-18 23:21:54 +0900

    [주문 도메인 개발] 주문 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,7 +2,9 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.LAZY;
@@ -10,6 +12,7 @@ import static jakarta.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -27,6 +30,10 @@ public class OrderItem {
 
     private int orderPrice;  //주문 가격
     private int count;  //주문 수량
+
+    //@NoArgsConstructor(access = AccessLevel.PROTECTED)로 대체(같은 의미)
+//    protected OrderItem() {
+//    }
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
