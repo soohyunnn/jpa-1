e74fdae0aab801faaeb4dffe2d64628d22f104c3
soohyunnn <sksmsqodn20@gmail.com>
2023-05-18 22:56:48 +0900

    [주문 도메인 개발] 주문, 주문상품 엔티티 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -28,4 +28,31 @@ public class OrderItem {
     private int orderPrice;  //주문 가격
     private int count;  //주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        //여기서는 로직이 간단하지만 실제로는 복잡해서 OrderItem 생성자에 넣어서 OrderItem을 생성하지 않는다.
+        //실무에서는 할인 로직 등등이 적용될 수 있기 때문에 그 부분을 고려한 코드이다.
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문상품 전체 가격 조회
+     * @return
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() + getCount();
+    }
 }
